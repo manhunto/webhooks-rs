dc1bc34495a1a5c444d6e1090010fb12d37096a7
Kuba Sładek <jakub.sladek@zoho.com>
2024-03-05 21:39:43 +0100

    feat(sdk): add create application method

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -1,8 +1,69 @@
-pub struct WebhooksSDK;
+use serde::de::DeserializeOwned;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+
+pub struct WebhooksSDK {
+    api_url: String,
+}
 
 impl WebhooksSDK {
     #[allow(dead_code)]
-    fn new() -> Self {
-        Self {}
+    fn new(api_url: String) -> Self {
+        Self { api_url }
+    }
+
+    fn application(&self) -> Application {
+        Application {
+            client: Client::new(self.api_url.to_string()),
+        }
+    }
+}
+
+struct Client {
+    api_url: String,
+}
+
+impl Client {
+    fn new(api_url: String) -> Self {
+        Self { api_url }
+    }
+
+    async fn post<I, O>(&self, url: String, body: I) -> O
+    where
+        I: Serialize,
+        O: DeserializeOwned,
+    {
+        let url = format!("{}/{}", self.api_url, url);
+
+        let response = reqwest::Client::new()
+            .post(url)
+            .json(&body)
+            .send()
+            .await
+            .unwrap(); // todo handle errors
+
+        response.json::<O>().await.unwrap() // todo handle errors
+    }
+}
+
+struct Application {
+    client: Client,
+}
+
+#[derive(Deserialize)]
+struct App {
+    id: String,
+    name: String,
+}
+
+impl Application {
+    pub async fn create(&self, name: String) -> App {
+        let body = json!({
+            "name": name,
+        });
+        let url = "v1/application";
+        let app: App = self.client.post(url.to_string(), body).await;
+
+        app
     }
 }
