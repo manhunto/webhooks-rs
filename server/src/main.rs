171dff3b23507abee27ad3c557fc9b601a3a1f78
Kuba Sładek <jakub.sladek@zoho.com>
2024-03-05 20:30:29 +0100

    feat(server): add application endpoint and persist apps in memory

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,76 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::web::Data;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
+use uuid::Uuid;
+
+#[derive(Debug, Clone)]
+struct Application {
+    #[allow(dead_code)]
+    id: Uuid,
+}
+
+impl Application {
+    fn new() -> Self {
+        Self { id: Uuid::new_v4() }
+    }
+}
+
+struct Storage {
+    applications: Mutex<Vec<Application>>,
+}
+
+impl Storage {
+    fn new() -> Self {
+        Self {
+            applications: Mutex::new(vec![]),
+        }
+    }
+
+    fn add_application(&self, app: Application) {
+        let mut applications = self.applications.lock().unwrap();
+
+        applications.push(app);
+    }
+
+    fn application_count(&self) -> usize {
+        let applications = self.applications.lock().unwrap();
+
+        applications.len()
+    }
+}
+
+async fn create_application(storage: Data<Storage>) -> impl Responder {
+    let app = Application::new();
+
+    storage.add_application(app.clone());
+
+    println!(
+        "Application created: {:?}, count: {}",
+        app,
+        storage.application_count()
+    );
+
+    HttpResponse::NoContent()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let ip = "127.0.0.1";
+    let port = 8090;
+
+    println!(
+        "Webhooks server is listening for requests on {}:{}",
+        ip, port
+    );
+
+    let storage = Data::new(Storage::new());
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(storage.clone())
+            .route("/api/v1/application", web::post().to(create_application))
+    })
+    .bind((ip, port))?
+    .run()
+    .await
 }
