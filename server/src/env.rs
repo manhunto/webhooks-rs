da3515c46c81d49add8d71e355b7977f03cce207
Kuba Sładek <jakub.sladek@zoho.com>
2024-04-14 22:59:55 +0200

    test(server): add tests for env module

--- server/src/env.rs
+++ server/src/env.rs
@@ -19,9 +19,71 @@ where
     T: Debug + FromStr,
     <T as FromStr>::Err: Debug,
 {
-    get_env(key).map_err(|_| default).unwrap().parse().unwrap()
+    if let Ok(var) = get_env(key) {
+        return var.parse().unwrap();
+    }
+
+    default
 }
 
 fn get_env(key: &str) -> Result<String, VarError> {
     env::var(key)
+}
+
+#[cfg(test)]
+mod tests {
+    use temp_env::{with_var, with_var_unset};
+
+    use crate::env::{env, env_with_default};
+
+    #[test]
+    fn env_with_default_env_is_set() {
+        with_var("TEST_STRING", Some("FOO"), || {
+            assert_eq!("FOO", env_with_default("TEST_STRING", "BAR".to_string()));
+        });
+
+        with_var("TEST_INT", Some("123"), || {
+            let var: i32 = env_with_default("TEST_INT", 456);
+            assert_eq!(123, var);
+        });
+    }
+
+    #[test]
+    fn env_with_default_env_is_not_set_then_pick_default() {
+        assert_eq!("BAR", env_with_default("TEST_STRING", "BAR".to_string()));
+
+        let var: i32 = env_with_default("TEST_INT", 456);
+        assert_eq!(456, var);
+    }
+
+    #[test]
+    fn env_when_env_is_set() {
+        with_var("TEST_STRING", Some("FOO"), || {
+            let var: String = env("TEST_STRING");
+
+            assert_eq!(String::from("FOO"), var);
+        });
+
+        with_var("TEST_INT", Some("132"), || {
+            let var: i32 = env("TEST_INT");
+
+            assert_eq!(132, var);
+        });
+    }
+
+    #[test]
+    #[should_panic(expected = "env TEST_STRING is not configured")]
+    fn env_without_string_env() {
+        with_var_unset("TEST_STRING", || {
+            let _: String = env("TEST_STRING");
+        })
+    }
+
+    #[test]
+    #[should_panic(expected = "env TEST_INT is not configured")]
+    fn env_without_int_env() {
+        with_var_unset("TEST_INT", || {
+            let _: u32 = env("TEST_INT");
+        })
+    }
 }
