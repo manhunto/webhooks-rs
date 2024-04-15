9af82ab1d5df948b3183a884293bfb8005b9a3d3
Kuba Sładek <jakub.sladek@zoho.com>
2024-04-15 20:08:14 +0200

    feat(server): handle bool envs

--- server/src/env.rs
+++ server/src/env.rs
@@ -48,9 +48,30 @@ impl EnvVar<u16> for Env {
     }
 }
 
+impl EnvVar<bool> for Env {
+    fn env(key: &str) -> bool {
+        match_bool(<Self as EnvVar<String>>::env(key).as_str(), key)
+    }
+
+    fn env_or(key: &str, default: bool) -> bool {
+        match_bool(
+            <Self as EnvVar<String>>::env_or(key, default.to_string()).as_str(),
+            key,
+        )
+    }
+}
+
+fn match_bool(value: &str, key: &str) -> bool {
+    match value {
+        "true" | "1" | "yes" => true,
+        "false" | "0" | "no" => false,
+        _ => panic!("`{}` is unrecognized bool value in {} env", value, key),
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use temp_env::{with_var, with_var_unset};
+    use temp_env::{with_var, with_var_unset, with_vars, with_vars_unset};
 
     use crate::env::{Env, EnvVar};
 
@@ -64,14 +85,37 @@ mod tests {
             let var: usize = Env::env_or("TEST_INT", 456);
             assert_eq!(123, var);
         });
+
+        with_vars(
+            [
+                ("BOOL_TRUE", Some("true")),
+                ("BOOL_FALSE", Some("false")),
+                ("INT_TRUE", Some("1")),
+                ("INT_FALSE", Some("0")),
+                ("STRING_TRUE", Some("yes")),
+                ("STRING_FALSE", Some("no")),
+            ],
+            || {
+                assert!(Env::env_or("BOOL_TRUE", false));
+                assert!(!Env::env_or("BOOL_FALSE", true));
+                assert!(Env::env_or("INT_TRUE", false));
+                assert!(!Env::env_or("INT_FALSE", true));
+                assert!(Env::env_or("STRING_TRUE", false));
+                assert!(!Env::env_or("STRING_FALSE", true));
+            },
+        );
     }
 
     #[test]
     fn env_with_default_env_is_not_set_then_pick_default() {
-        assert_eq!("BAR", Env::env_or("TEST_STRING", "BAR".to_string()));
+        with_vars_unset(["TEST_STRING", "TEST_INT", "TEST_BOOL"], || {
+            assert_eq!("BAR", Env::env_or("TEST_STRING", "BAR".to_string()));
 
-        let var: usize = Env::env_or("TEST_INT", 456);
-        assert_eq!(456, var);
+            let var: usize = Env::env_or("TEST_INT", 456);
+            assert_eq!(456, var);
+
+            assert!(Env::env_or("TEST_BOOL", true));
+        });
     }
 
     #[test]
@@ -87,6 +131,36 @@ mod tests {
 
             assert_eq!(132, var);
         });
+
+        with_vars(
+            [
+                ("BOOL_TRUE", Some("true")),
+                ("BOOL_FALSE", Some("false")),
+                ("INT_TRUE", Some("1")),
+                ("INT_FALSE", Some("0")),
+                ("STRING_TRUE", Some("yes")),
+                ("STRING_FALSE", Some("no")),
+            ],
+            || {
+                let val: bool = Env::env("BOOL_TRUE");
+                assert!(val);
+
+                let val: bool = Env::env("BOOL_FALSE");
+                assert!(!val);
+
+                let val: bool = Env::env("INT_TRUE");
+                assert!(val);
+
+                let val: bool = Env::env("INT_FALSE");
+                assert!(!val);
+
+                let val: bool = Env::env("STRING_TRUE");
+                assert!(val);
+
+                let val: bool = Env::env("STRING_FALSE");
+                assert!(!val);
+            },
+        );
     }
 
     #[test]
@@ -102,6 +176,22 @@ mod tests {
     fn env_without_int_env() {
         with_var_unset("TEST_INT", || {
             let _: usize = Env::env("TEST_INT");
+        })
+    }
+
+    #[test]
+    #[should_panic(expected = "env TEST_BOOL is not configured")]
+    fn env_without_bool_env() {
+        with_var_unset("TEST_BOOL", || {
+            let _: bool = Env::env("TEST_BOOL");
+        })
+    }
+
+    #[test]
+    #[should_panic(expected = "`FOO` is unrecognized bool value in TEST_BOOL env")]
+    fn env_unrecognized_bool_value() {
+        with_var("TEST_BOOL", Some("FOO"), || {
+            let _: bool = Env::env("TEST_BOOL");
         })
     }
 }
