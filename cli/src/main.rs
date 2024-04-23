d0d4b4f739c403f4b6b8121704741985577af43b
Kuba Sładek <jakub.sladek@zoho.com>
2024-04-23 20:56:50 +0200

    feat(cli): add cli app skeleton

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,37 @@
+use clap::{Parser, Subcommand};
+
+/// Cli app to manage webhook-rs server
+#[derive(Debug, Parser)]
+#[clap(name = "webhooks-cli", version, about)]
+pub struct App {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Clone, Debug, Subcommand)]
+enum Command {
+    /// Resource for application management
+    App {
+        #[clap(subcommand)]
+        subcommand: AppSubcommand,
+    },
+}
+
+#[derive(Clone, Debug, Subcommand)]
+enum AppSubcommand {
+    /// Creates an application
+    Create {
+        /// App name
+        name: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::parse();
+
+    match app.command {
+        Command::App { subcommand } => match subcommand {
+            AppSubcommand::Create { name } => println!("Creating app with name {}", name),
+        },
+    }
 }
