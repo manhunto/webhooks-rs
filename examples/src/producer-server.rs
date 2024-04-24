1c4886dc7f6c16bbd17a94baef06fc98b9a85770
Kuba Sładek <jakub.sladek@zoho.com>
2024-04-24 20:59:16 +0200

    fix(example): handle errors from sdk

--- examples/src/producer-server.rs
+++ examples/src/producer-server.rs
@@ -2,10 +2,11 @@ use std::env;
 
 use dotenv::dotenv;
 
+use sdk::error::Error;
 use sdk::WebhooksSDK;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
     dotenv().ok();
 
     let url: String = env::var("SERVER_URL").unwrap();
@@ -13,7 +14,9 @@ async fn main() {
     println!("{}", url);
 
     let sdk = WebhooksSDK::new(url.as_str());
-    let app = sdk.application().create("dummy").await;
+    let app = sdk.application().create("dummy").await?;
 
     println!("App created - {:?}", app);
+
+    Ok(())
 }
