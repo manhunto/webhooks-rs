1f8924d5e25d0bf73ce9cd8f3a1cc47f13f01002
Kuba Sładek <jakub.sladek@zoho.com>
2024-04-29 22:19:12 +0200

    test(server): add tests for ksuid type

--- server/src/types/mod.rs
+++ server/src/types/mod.rs
@@ -7,6 +7,7 @@ macro_rules! make_ksuid {
 
         impl $name {
             const PREFIX: &'static str = $prefix;
+            const TERMINATOR: char = '_';
 
             pub fn new() -> Self {
                 use svix_ksuid::{Ksuid, KsuidLike};
@@ -19,7 +20,7 @@ macro_rules! make_ksuid {
 
         impl std::fmt::Display for $name {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                write!(f, "{}_{}", Self::PREFIX, self.id)
+                write!(f, "{}{}{}", Self::PREFIX, Self::TERMINATOR, self.id)
             }
         }
 
@@ -27,14 +28,26 @@ macro_rules! make_ksuid {
             type Error = String;
 
             fn try_from(value: String) -> Result<Self, Self::Error> {
-                use itertools::Itertools;
                 use std::str::FromStr;
 
-                let (prefix, id) = value.split_terminator('_').collect_tuple().unwrap();
+                Self::from_str(value.as_str())
+            }
+        }
+
+        impl std::str::FromStr for $name {
+            type Err = String;
+
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                use itertools::Itertools;
+
+                let (prefix, id) = s
+                    .split_terminator(Self::TERMINATOR)
+                    .collect_tuple()
+                    .unwrap();
 
                 if prefix != Self::PREFIX {
                     return Err(format!(
-                        "{} should have prefix {} but have {}",
+                        "'{}' type should have prefix '{}' but have '{}'",
                         stringify!($name),
                         Self::PREFIX,
                         prefix,
@@ -59,3 +72,51 @@ make_ksuid!(MessageId, "msg");
 make_ksuid!(RoutedMessageId, "rmsg");
 make_ksuid!(ApplicationId, "app");
 make_ksuid!(EndpointId, "ep");
+
+#[cfg(test)]
+mod ksuid_tests {
+    use std::str::FromStr;
+
+    use itertools::Itertools;
+    use svix_ksuid::{Ksuid, KsuidLike};
+
+    make_ksuid!(TestId, "test");
+
+    #[test]
+    fn can_be_build_from_string() {
+        assert!(TestId::from_str("test_1srOrx2ZWZBpBUvZwXKQmoEYga2").is_ok())
+    }
+
+    #[test]
+    fn cannot_build_from_invalid_prefix() {
+        let ksuid = Ksuid::new(None, None);
+        let id = format!("invalid_{}", ksuid);
+
+        assert_eq!(
+            Err("'TestId' type should have prefix 'test' but have 'invalid'".to_string()),
+            TestId::try_from(id)
+        );
+    }
+
+    #[test]
+    fn eq_test() {
+        let a = TestId::from_str("test_1srOrx2ZWZBpBUvZwXKQmoEYga2").unwrap();
+        let b = TestId::from_str("test_1srOrx2ZWZBpBUvZwXKQmoEYga2").unwrap();
+        let c = TestId::from_str("test_0ujtsYcgvSTl8PAuAdqWYSMnLOv").unwrap();
+
+        assert!(a.eq(&b));
+        assert!(a.eq(&a));
+        assert!(!a.eq(&c));
+        assert!(!b.eq(&c));
+    }
+
+    #[test]
+    fn display_with_prefix() {
+        let sut = TestId::new();
+
+        let binding = sut.to_string();
+        let (prefix, _) = binding.split_terminator('_').collect_tuple().unwrap();
+
+        assert_eq!("test", prefix);
+    }
+}
