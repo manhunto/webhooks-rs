3404b27922baff94c179a6d36eadc044497ff2a5
Kuba Sładek <jakub.sladek@zoho.com>
2024-05-21 17:35:53 +0200

    build: fix executing sqlx

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -7,7 +7,7 @@ source .env
 echo "Run migrations";
 
 RETRIES=30
-until cargo sqlx migrate run --source=server/migrations || [ $RETRIES -eq 0 ];
+until sqlx migrate run --source=server/migrations || [ $RETRIES -eq 0 ];
 do
   echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
   sleep 1;
