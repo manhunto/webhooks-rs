f4de78e7881b306e1d5381ce2ac36531da9afcbb
Kuba Sładek <jakub.sladek@zoho.com>
2024-06-04 16:54:27 +0200

    feat(cli): add endpoint create boilerplate

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -8,7 +8,7 @@ use sdk::WebhooksSDK;
 /// Cli app to manage webhook-rs server
 #[derive(Debug, Parser)]
 #[clap(name = "webhooks-cli", version, about)]
-pub struct App {
+pub struct Cli {
     #[clap(subcommand)]
     command: Command,
 }
@@ -16,18 +16,34 @@ pub struct App {
 #[derive(Clone, Debug, Subcommand)]
 enum Command {
     /// Resource for application management
-    App {
+    Application {
         #[clap(subcommand)]
-        subcommand: AppSubcommand,
+        subcommand: ApplicationSubcommand,
+    },
+    /// Resource for endpoints management
+    Endpoint {
+        #[clap(subcommand)]
+        subcommand: EndpointSubcommand,
     },
 }
 
 #[derive(Clone, Debug, Subcommand)]
-enum AppSubcommand {
+enum ApplicationSubcommand {
     /// Creates an application
     Create {
-        /// App name
+        /// Application name
         name: String,
+    },
+}
+
+#[derive(Clone, Debug, Subcommand)]
+enum EndpointSubcommand {
+    /// Creates an endpoint
+    Create {
+        app_id: String,
+        url: String,
+        #[arg(value_parser, num_args = 1.., value_delimiter = ',', required = true)]
+        topics: Vec<String>,
     },
 }
 
@@ -35,18 +51,41 @@ enum AppSubcommand {
 async fn main() {
     dotenv().ok();
 
-    let app = App::parse();
+    let cli = Cli::parse();
 
     let url = env::var("SERVER_URL").expect("env SERVER_URL is not set");
     let sdk = WebhooksSDK::new(url.as_str());
 
-    match app.command {
-        Command::App { subcommand } => match subcommand {
-            AppSubcommand::Create { name } => {
+    match cli.command {
+        Command::Application { subcommand } => match subcommand {
+            ApplicationSubcommand::Create { name } => {
                 let app = sdk.application().create(name.as_str()).await.unwrap();
 
                 println!("App {} with name '{}' has been created", app.id, app.name);
             }
         },
+        Command::Endpoint { subcommand } => match subcommand {
+            EndpointSubcommand::Create {
+                app_id,
+                url,
+                topics,
+            } => {
+                println!("{}", app_id);
+                println!("{}", url);
+                println!("{:?}", topics);
+            }
+        },
     };
+}
+
+#[cfg(test)]
+mod test {
+    use clap::CommandFactory;
+
+    use crate::Cli;
+
+    #[test]
+    fn verify_cli() {
+        Cli::command().debug_assert()
+    }
 }
